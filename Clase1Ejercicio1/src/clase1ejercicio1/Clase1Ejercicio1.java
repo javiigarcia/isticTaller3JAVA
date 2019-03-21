c028795a51a6645278e5fda10bb411ba6afa053e
javiigarcia <48774541+javiigarcia@users.noreply.github.com>
2019-03-20 21:12:15 -0300

    Ejercicio numero primo
    Ejercicio numero primo

---
+++ Clase1Ejercicio1/src/clase1ejercicio1/Clase1Ejercicio1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC
+ */
+public class Clase1Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("Ingrese un numero para verificar si es primo");
+        Scanner miscanner = new Scanner (System.in);
+        int i;
+        int numero ;
+        numero =  miscanner.nextInt();
+        
+        for (i=0 ; 1 < numero ; i ++)
+        {
+            //System.out.println(i);  
+             if (numero % i = = 0)
+             {
+            
+                    
+        }
+  
+    
+    }
+}
